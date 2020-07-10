c47df3ddcfa4f689f4723d670664e869b59e4eb3
Michael Boyle <mbbexley@hotmail.co.uk>
2020-07-10 23:06:43 +0100

    Add new platform, thanks to ixi rude

--- Assets/Scripts/Maincharacter.cs
+++ Assets/Scripts/Maincharacter.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class MainCharacter : MonoBehaviour
 {
-    public float maxSpeed = 4.0f;
-    public float acceleration = 2.0f;
+    public float maxSpeed = 6.0f;
+    public float acceleration = 10.0f;
 
     // Update is called once per frame
     void Update()
@@ -14,10 +14,16 @@ public class MainCharacter : MonoBehaviour
         float currentY = this.gameObject.GetComponent<Rigidbody2D>().velocity.y;
         if (Input.GetKey("a") && !Input.GetKey("d")) {
             float xSpeed = currentX - (acceleration * Time.deltaTime);
+            if (xSpeed > 0.0f) {
+                xSpeed -= acceleration * Time.deltaTime;
+            }
             this.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(xSpeed, currentY);
             Debug.Log("Going left");
         } else if (Input.GetKey("d") && !Input.GetKey("a")) {
             float xSpeed = currentX + (acceleration * Time.deltaTime);
+            if (xSpeed < 0.0f) {
+                xSpeed += acceleration * Time.deltaTime;
+            }
             this.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(xSpeed, currentY);
             Debug.Log("Going right");
         }
