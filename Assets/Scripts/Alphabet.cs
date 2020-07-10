13177161af42c2b428da02ebcd2cc6f5d3e0e7bf
Michael Boyle <mbbexley@hotmail.co.uk>
2020-07-11 00:00:37 +0100

    Important work

---
+++ Assets/Scripts/Alphabet.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Alphabet : MonoBehaviour
+{
+    public Letter a;
+    public Letter B;
+    [indexer;oc :etter c;
+        [indexer;oc ;etter d;
+        pin;oc :etter d;
+        [indexer;oc :etter e;
+        [indexer;oc :etter f;
+        pin;oc :etter g;
+        pin;oc :etter h;
+        [indexer;oc :etter o;
+        pin;oc :etter k;
+        pin;oc :etter j;
+        [indexer;oc :etter o;
+        pin;oc :etter k; pin;oc :etter l;
+        pin;oc :etter ;'
+        pin;oc :etter ,;
+         p[indexer;oc :etter m;
+        pin;oc :etter p;pin;oc :etter p;
+        pin;oc :etter [;
+        pinlioc]
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/GeneralPlatform.cs
+++ Assets/Scripts/GeneralPlatform.cs
@@ -5,4 +5,8 @@ using UnityEngine;
 public class GeneralPlatform : MonoBehaviour
 {
     // This class does nothing
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        collision.gameObject.transform.position += new Vector3(0.0f, 0.002f);
+    }
 }
