09b7eb2690c195ef60946179081a4866e1dfa4de
Michael Boyle <mbbexley@hotmail.co.uk>
2020-07-11 11:53:51 +0100

    Add sentence with random word platform placement

---
+++ Assets/Scripts/Sentence.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sentence : MonoBehaviour
+{
+    public string sentence;
+    public float maxHeightDifference = 3.0f;
+    public float maxJumpRange = 3.0f;
+    public GameObject word; // Prefab word
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float x = 0;
+        string[] words = sentence.Split(' ');
+        for (int i = 0; i < words.Length; ++i) {
+            word.GetComponent<Word>().word = words[i];
+            GameObject go = GameObject.Instantiate(word, gameObject.transform);
+            float width = go.GetComponent<Text>().preferredWidth;
+            x += (width / 2) / 100;
+            float heightChange = maxHeightDifference / 2.0f;
+            float y = Random.Range(i*heightChange, i* heightChange + heightChange);
+            go.GetComponent<RectTransform>().localPosition = new Vector3(x, y);
+            x += (width / 2) / 100 + Random.Range(0.6f, maxJumpRange);
+            Debug.Log("x is now " + x);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Word.cs
+++ Assets/Scripts/Word.cs
@@ -8,17 +8,21 @@ public class Word : MonoBehaviour
     public string word;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         Text text = gameObject.GetComponent<Text>();
         text.text = word;
         Debug.Log("The preferred width of " + text.text + " is " + text.preferredWidth);
-        RectTransform rt = GetComponent<RectTransform>();
-        rt.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, text.preferredHeight);
-        rt.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, text.preferredWidth);
-        BoxCollider2D bc = GetComponent<BoxCollider2D>();
-        bc.size = new Vector2 (text.preferredWidth, text.preferredHeight);
+    }
 
+    private void Start()
+    {
+        Text text = gameObject.GetComponent<Text>();
+        RectTransform rt = GetComponent<RectTransform>();
+        rt.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, text.preferredWidth);
+        rt.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, text.preferredHeight);
+        BoxCollider2D bc = GetComponent<BoxCollider2D>();
+        bc.size = new Vector2(text.preferredWidth, text.preferredHeight);
     }
 
     // Update is called once per frame
