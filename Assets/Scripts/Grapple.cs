58421470f8161dbb1bd7f082b5797896c9da76f9
Michael Boyle <mbbexley@hotmail.co.uk>
2020-07-11 17:59:47 +0100

    Get grapple firing in the right direction

--- Assets/Scripts/Grapple.cs
+++ Assets/Scripts/Grapple.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class Grapple : MonoBehaviour
 {
     public GameObject hookPrefab;
-    public float range = 10.0f;
-    public float fireSpeed = 8.0f;
+    public float range = 8.0f;
+    public float fireSpeed = 14.0f;
     public float reelSpeed = 2.0f;
     private bool firing = false;
     private bool reeling = false;
@@ -31,14 +31,27 @@ public class Grapple : MonoBehaviour
             Debug.Log("Direction to grapple is (" + direction.x + ", " + direction.y + ")");
             firing = true;
             firedDistance = 0.0f;
-            hook = GameObject.Instantiate(hookPrefab, transform.position, Quaternion.LookRotation(direction)).GetComponent<Hook>();
+            hook = GameObject.Instantiate(
+                hookPrefab, 
+                transform.position, 
+                Quaternion.Euler(0.0f, 0.0f, Mathf.Rad2Deg*Mathf.Atan2(direction.y,direction.x)+-90.0f)
+            ).GetComponent<Hook>();
             reeling = false;
+            gameObject.GetComponent<LineRenderer>().enabled = true;
         } else if (reeling) {
 
         }
 
         if (firing) {
-
+            Vector3 movement = direction.normalized * fireSpeed * Time.deltaTime;
+            if (movement.magnitude + firedDistance > range) {
+                firing = false;
+                GameObject.Destroy(hook.gameObject);
+                gameObject.GetComponent<LineRenderer>().enabled = false;
+            } else {
+                hook.transform.position += movement;
+                firedDistance += movement.magnitude;
+            }
         }
 
         if (hook) {
