ca211ccda4716dd78d000603fc742d527d8bb51f
Michael Boyle <mbbexley@hotmail.co.uk>
2020-07-11 11:00:53 +0100

    Add text box

---
+++ Assets/Scripts/Word.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Word : MonoBehaviour
+{
+    public string word;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Text text = gameObject.GetComponent<Text>();
+        text.text = word;
+        Debug.Log("The preferred width of " + text.text + " is " + text.preferredWidth);
+        RectTransform rt = GetComponent<RectTransform>();
+        rt.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, text.preferredHeight);
+        rt.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, text.preferredWidth);
+        BoxCollider2D bc = GetComponent<BoxCollider2D>();
+        bc.size = new Vector2 (text.preferredWidth, text.preferredHeight);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
